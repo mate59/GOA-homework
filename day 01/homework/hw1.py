167a19254bed6b3eb7e42db6358e4b45171675b7
mate59 <mate260811@gmail.com>
2025-07-03 14:23:39 +0400

    1

--- day 01/homework/hw1.py
+++ day 01/homework/hw1.py
@@ -1,18 +1,18 @@
 
 #string არის ტექსტი, რომელიც მოცემულია ბრჭყალებში მაგალითად
-"Mate Arabuli"
+print("Mate Arabuli")
 
 #integer არის მთელი რიცხვი მაგალითად
-13
+print(13)
 
 #float არის ათწილადი მაგალითად
-1.73
+print(1.73)
 
 print("Mate Arabuli")  #str
 print(13)  #int
 print(1.73)  #fl
 
 #ცვლადი არის სახელი, სადაცაა მეხსიერებაში შენახული ინფორმაცია მაგალითად
-name="Mate"
-age=13
-height="1.73"
+name = "Mate"
+age = 13
+height = 1.73
