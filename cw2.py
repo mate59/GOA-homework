ed53e3958445179efe2b9aec7133540be44d2c19
mate59 <mate260811@gmail.com>
2025-07-06 16:09:24 +0400

    3

---
+++ cw2.py
@@ -0,0 +1,23 @@
+#print(10+5)
+#print(50-32)
+#print(5*4)
+#print(45/8)
+
+
+#name = "mate"
+
+#print(name)
+
+#mb1 = 26
+
+#mb2 = 44
+
+#print(mb1 + mb2)
+
+#ingis da intejeris ertad damateba ar sheidzleba
+
+num1 = 120
+
+num2 = 80
+
+print(num1+num2)
---
+++ day 02/hw2.py
@@ -0,0 +1,50 @@
+num1 = 65
+
+num2 = 35
+
+print(num1+num2)   
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+
+
+age1 = 10
+age2 = 13
+age3 = 12
+age4 = 14
+
+print(age4)
+
+pocketmoney1 = 3.80
+pocketmoney2 = 5.20
+pocketmoney3 = 11.20
+pocketmoney4 = 50.5
+
+print(pocketmoney3)
+
+car1 = "volkswagen"
+car2 = "bmw"
+car3 = "audi"
+car4 = "skoda"
+
+print(car1)
+
+number1 = 120
+number2 = 80
+
+print(number1 + number2)
+
+name1 = "Mate"
+name2 = "nika"
+name3 = "sandro"
+name4 = "saba"
+
+print(name1 + name2 + name3 + name4)
+
+age = 13
+
+height = 1.73
+
+print(age - height)
+print(age + height)
+print(age * height)
