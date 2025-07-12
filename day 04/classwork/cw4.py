13dbdeecffc25d54cd5145b95b09ad2b64f53e86
mate59 <mate260811@gmail.com>
2025-07-12 23:12:13 +0400

    1

---
+++ day 04/classwork/cw4.py
@@ -0,0 +1,17 @@
+name = input("mate")
+
+print(type(name))
+
+print(type(50))
+print(type(35))
+
+
+brand = ("volkswagen")
+height = (1.75)
+age = 14
+horse_power = 459
+
+print(type(brand))
+print(type(height))
+print(type(age))
+print(type(horse_power))
