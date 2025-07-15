217bd43343b4b1abb9096f59229ce8811f1ff46c
mate59 <mate260811@gmail.com>
2025-07-16 00:40:29 +0400

    1

---
+++ day 05/classwork/hw05.py
@@ -0,0 +1,16 @@
+#შედარება
+
+print(10>5)#true
+print(10<5)#false
+print(14==15)#false
+
+#მინიჭების ოპერატორი
+
+num1 = 20
+
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+
+print(num1>num2)
+print(num1<num2)
+print(num1=num2)
