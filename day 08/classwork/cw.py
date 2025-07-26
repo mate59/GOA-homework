ee0569fc920b8277cd5da013be00e19489694d0a
mate59 <mate260811@gmail.com>
2025-07-26 22:36:26 +0400

    2

---
+++ day 08/classwork/cw.py
@@ -0,0 +1,15 @@
+# and = false
+
+# or = true
+
+
+# print(50 > 30 or 30 == 31)
+# print(25 < 39 or 10 < 8)
+# print(30 == 30 or 20 < 17)
+
+# print(15 < 60 and 70 == 69)
+# print(18 == 20 and 60 > 20)
+# print(164 < 170 and 1 == 2)
+
+for i in range(10000000000000000000000000000000000000000000): 
+    print("mate")
