1709641aae9a9c218a8da5e130712497d0fd70fd
mate59 <mate260811@gmail.com>
2025-07-29 15:09:17 +0400

    2

--- day 08/classwork/cw.py
+++ day 08/classwork/cw.py
@@ -1,19 +1,29 @@
-# and = false
+# # and = false
 
-# or = true
+# # or = true
 
 
-# print(50 > 30 or 30 == 31)
-# print(25 < 39 or 10 < 8)
-# print(30 == 30 or 20 < 17)
+# # print(50 > 30 or 30 == 31)
+# # print(25 < 39 or 10 < 8)
+# # print(30 == 30 or 20 < 17)
 
-# print(15 < 60 and 70 == 69)
-# print(18 == 20 and 60 > 20)
-# print(164 < 170 and 1 == 2)
+# # print(15 < 60 and 70 == 69)
+# # print(18 == 20 and 60 > 20)
+# # print(164 < 170 and 1 == 2)
 
-for i in range(20): 
-    print("mate arabuli")
+# for i in range(20): 
+#     print("mate arabuli")
 
-for i in range(10):
-    print(i)   #itvlis 0 dan 9mde
+# for i in range(10):
+#     print(i)   #itvlis 0 dan 9mde
+
+
+
+for i in range(100):
+    print("arabuli")
+    print(20)
+    name = "mate"
+    print(name)
+print("for loop ended")
+
 
