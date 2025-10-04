aadb7ef19e1d069b2da7dce42ff8c1d2c8548e8d
mate59 <mate260811@gmail.com>
2025-10-04 22:52:59 +0400

    4

---
+++ day 28/homework/hw.py
@@ -0,0 +1,14 @@
+list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,]
+
+print(list[5:11])
+
+
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+print(list1[-20:-9])
+
+
+list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+second_number = int(input("enter a second nuber (from 3 to 11): "))
+print(list2[1:second_number])
