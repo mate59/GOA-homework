51ffde100f74319dd08b5d8d50991a43a13a32df
mate59 <mate260811@gmail.com>
2025-10-21 17:16:58 +0400

    1

--- day 32/homework/hw.py
+++ day 32/homework/hw.py
@@ -24,13 +24,13 @@ for i in range(1, 50):
 
 for i in range(20):
 
-    if i / 3:
+    if i % 3:
         print("იყოფა 3-ზე")
 
-    elif i / 5:
+    elif i % 5:
         print("იყოფა 5-ზე")
 
-    elif i / 3 and i / 5:
+    elif i % 3 and i % 5:
         print("იყოფა 3-ზე და 5-ზე")
 
     else:
