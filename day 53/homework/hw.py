5626955e9a4f8dbf9332bd1c75b688c7020410b5
mate59 <mate260811@gmail.com>
2026-01-10 18:16:44 +0400

    1

--- day 53/homework/hw.py
+++ day 53/homework/hw.py
@@ -1,10 +1,18 @@
 # 2) შექმენით სიტყვებით სავსე სია, თუ სიტყვის ყველა ასო არის პატარა და პირველი ასო არის g, მაშინ ახალ სიაში ჩაამატეთ სახელი "Goga", თუ სიტყვის ყველა ასო არის დიდი ან იწყება ასო N-თი, მაშინ სიაში ჩაამატეთ სახელი "Nika", სხვა შემთხვევაში სიაში ჩაამატეთ სიტყვა "ლიდერი". დაპრინტეთ მიღებული სია.
 
-words = ["keyboard" , "Mause"]
+words = ["keyboard" , "Mouse" , "goga" , "BMW"]
 
+newlist = []
 
+for i in words:
+    if i.islower() and i[0] == "g":
+        newlist.append("Goga")
+    elif i.isupper() or i[0] == "N":
+        newlist.append("Nika")
+    else:
+        newlist.append("ლიდერი")
 
-
+print(newlist)
 
 
 
