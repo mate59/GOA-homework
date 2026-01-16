c15cca812674bc9e586283e4c0f97a10e7ddd62a
mate59 <mate260811@gmail.com>
2026-01-16 23:26:42 +0400

    1

---
+++ py.py
@@ -0,0 +1,30 @@
+num = ["gela", "Keyboard", "mouse", "Mate", "Nika"]
+
+num1 = []
+
+i = 0
+
+while i < len(num):
+    if num[i] == num[i].capitalize() and i % 2 == 0:
+        num1.append(num[i])
+    i += 1
+
+print(num1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
