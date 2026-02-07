0d1562580505a3a4bdbfda4cc81f63e38f7dd476
mate59 <mate260811@gmail.com>
2026-02-07 22:58:57 +0400

    2

---
+++ day 64/classwork/cw.py
@@ -0,0 +1,20 @@
+def say_my_name():
+    print("my name is mate")
+
+say_my_name()
+say_my_name()
+
+
+def check_even():
+
+    num = 23
+
+    if num % 2 == 0:
+        return "this number is even"
+    
+    else:
+        return "this number is odd"
+    
+
+print(check_even())
+
