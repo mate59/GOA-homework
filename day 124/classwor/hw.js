e20e0cacaf8a213c8f459be2e45ef94e1d170579
mate59 <mate260811@gmail.com>
2026-09-05 14:50:49 +0400

    2

---
+++ day 124/classwor/hw.js
@@ -0,0 +1,23 @@
+console.log('mate arabuli' .toUpperCase())
+
+console.log('saqartvelo' .toLowerCase())
+
+console.log('   mate   ' .trim())
+
+console.log(2 + 2)
+
+console.log(2 - 2)
+
+console.log(2 / 2)
+
+console.log(2 * 2)
+
+console.log(20 % 7)
+
+//=========================================================================
+
+console.log("i live in Tbilisi" .replace("live", "love"))
+
+console.log("i live in Tbilisi and also i live in Georgia" .replaceAll("live", "love"))
+
+console.log("saqartvelo" .repeat(10))
