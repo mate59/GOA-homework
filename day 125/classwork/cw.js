78000b083fd80ad6f3810b6ae7734fa885edd790
mate59 <mate260811@gmail.com>
2026-09-08 21:21:53 +0400

    67

---
+++ day 125/classwork/cw.js
@@ -0,0 +1,37 @@
+let num = 10
+
+num += 10 // 20
+
+num -= 5  // 5
+
+num /= 2  // 5
+
+num *= 5  // 2
+
+num++     // 11
+
+num--     // 9
+
+
+let name = 'mate'
+
+let surname = 'arabuli'
+
+let age = '15'
+
+let hobby = 'biker'
+
+console.log(`hi my name is ${name} ${surname} and im ${age} years old and my hobby is ${hobby}`)
+
+
+let str = 'cw'
+
+let bol = true
+
+let num2 = 10
+
+console.log(typeof str)
+
+console.log(typeof bol)
+
+console.log(typeof num2)
